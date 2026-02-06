0a0659939f6083685d17dd3743c5c8c97ec65f6c
yyh061129vic-debug <yyh061129vic@gmail.com>
2026-02-06 02:35:26 +0000

    Add SQL query to list students and their courses

--- worksheet/task2/question4.sql
+++ worksheet/task2/question4.sql
@@ -1,3 +1,13 @@
 -- List all students along with the names of the courses they are enrolled in.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, CourseName
+SELECT
+  s.StudentId,
+  s.FirstName,
+  s.LastName,
+  c.CourseName
+FROM Student s
+JOIN Enrolment e
+  ON e.StudentId = s.StudentId
+JOIN Course c
+  ON c.CourseId = e.CourseId;
