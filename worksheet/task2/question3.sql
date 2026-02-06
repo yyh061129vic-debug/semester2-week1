bf2a2ab271f45b56483df482bf04587ff006904e
yyh061129vic-debug <yyh061129vic@gmail.com>
2026-02-06 02:34:32 +0000

    Add SQL query to calculate total enrolments by department

--- worksheet/task2/question3.sql
+++ worksheet/task2/question3.sql
@@ -1,3 +1,12 @@
 -- For each department, calculate the number of enrolments in its courses.
 -- Expected Columns:
 -- DepartmentName, TotalEnrolments
+SELECT
+  d.DepartmentName AS DepartmentName,
+  COUNT(*) AS TotalEnrolments
+FROM Department d
+JOIN Course c
+  ON c.DepartmentId = d.DepartmentId
+JOIN Enrolment e
+  ON e.CourseId = c.CourseId
+GROUP BY d.DepartmentName;
